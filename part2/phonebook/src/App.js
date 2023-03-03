cb596b9017587eb40ba1e95b1323da7bf4db7e31
fgrullon <frankgrullon@Franks-MacBook-Pro.local>
2023-03-03 09:48:11 -0400

    2.9 phonebook step4 allow filter numbers

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,10 +12,18 @@ const Numbers = ({ persons }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([{name : 'Arto Hellas'}]);
+  // const [persons, setPersons] = useState([{name : 'Arto Hellas'}]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
+  const [filteredPersons, setfilteredPersons] = useState(persons);
+  
 
   const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
+  const [newNumber, setNewNumber] = useState();
   
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -23,11 +31,24 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return ;
     }
-    setPersons(persons.concat({name : newName, number : newNumber}));
+    const newPersons = persons.concat({name : newName, number : newNumber});
+    setPersons(newPersons);
+    setfilteredPersons(newPersons);
+  }
+
+  const handleFilter = (event) => {
+    const newPersons = persons.filter((e) => e.name.toLowerCase().includes(event.target.value))
+
+    setfilteredPersons(newPersons);
   }
   return (
     <div>
       <h2>PhoneBook</h2>
+      <div>
+          filter shown with: <input onChange={handleFilter} />
+        </div>
+        <h2>add a new</h2>
+
       <form  onSubmit={handleSubmit}>
         <div>
           name: <input onChange={(event) => setNewName(event.target.value)} />
@@ -40,7 +61,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
+      <Numbers persons={filteredPersons} />
     </div>
   );
 }
