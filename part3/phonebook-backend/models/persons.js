6ebb78ef5a917ee2771cb8b2df1fdf930f7d9c64
fgrullon <franfelix@gmail.com>
2023-03-08 09:33:23 -0400

    3.20 phonebook database validate phone number format

--- part3/phonebook-backend/models/persons.js
+++ part3/phonebook-backend/models/persons.js
@@ -11,7 +11,17 @@ const personSchema = new mongoose.Schema({
         minLength : 3,
         required : true
     },
-    number: String
+    number: {
+        type: String,
+        minLength : 8,
+        validate: 
+        {
+          validator: function(v) {
+            return /\d{2}-\d{7}/.test(v) || /\d{3}-\d{8}/.test(v);
+          },
+          message: props => `${props.value} is not a valid phone number!`
+        }
+    }
 });
 
 
