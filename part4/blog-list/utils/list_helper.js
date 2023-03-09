edd955de29464841c332dff08adc71787e54fe9c
fgrullon <franfelix@gmail.com>
2023-03-09 05:44:50 -0400

    4.6 helper functions and unit test step4 return author with most blogs

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -10,6 +10,7 @@ const totalLikes = (blogs) => {
 }
 
 const favoriteBlog = (blogs) => {
+    if(blogs.length === 0) return {};
     const {title, author, likes} = blogs.reduce(function(prev, current) {
         return (prev.likes > current.likes) ? prev : current;
     });
@@ -17,8 +18,24 @@ const favoriteBlog = (blogs) => {
     return {title, author, likes};
 }
 
+const mostBlogs = (blogs) => {
+    
+    if(blogs.length === 0) return {};
+    const map = {};
+
+    blogs.forEach(element => {
+        map[element.author] = map[element.author] ? map[element.author] + 1 : 1;
+    });
+
+    const author = Object.entries(map).sort((a,b) => b[1] - a[1])[0];
+
+    return { author : author[0], blogs : author[1]};
+
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
