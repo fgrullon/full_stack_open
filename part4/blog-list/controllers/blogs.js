a4d4397a09cd8f8187c0500a3855d4b7c1f63300
fgrullon <franfelix@gmail.com>
2023-03-12 07:56:29 -0400

    4.20 bloglist expansion step8 refactor taking the token to a middleware

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -4,14 +4,6 @@ const User = require('../models/user');
 const usersRouter = require('./users');
 const jwt = require('jsonwebtoken');
 
-const getTokenFrom = req => {
-    const authorization = req.get('authorization');
-
-    if(authorization && authorization.startsWith('Bearer ')){
-        return authorization.replace('Bearer ', '');
-    }
-    return null;
-}
 
 
 blogsRouter.get('/', async (req, res, next) => {
@@ -26,9 +18,10 @@ blogsRouter.get('/', async (req, res, next) => {
 });
 
 blogsRouter.post('/', async (req, res, next) => {
+    console.log(req.token)
     try {
         const body = req.body;
-        const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+        const decodedToken = jwt.verify(req.token, process.env.SECRET)
         
         if(!decodedToken.id){
             return res.status(401).json({ error : 'token invalid' });
