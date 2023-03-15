795381ab35d22dd7b04db2681f8a6526330d3715
fgrullon <franfelix@gmail.com>
2023-03-15 16:01:48 -0400

    5.7 blog list frontend step7 added button to toggle blog details

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,43 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
+
+const Blog = ({blog}) => {
+
+  const [showDetail, setShowDetails] = useState(false);
+
+  const hide = { display: showDetail ? 'none' : '' }
+  const show = { display: showDetail ? '' : 'none' }
+
+  return (
+    <div className="blog"> 
+        <div>
+          {blog.title} {blog.author} 
+          
+          <button 
+            style={hide} 
+            onClick={() => setShowDetails(!showDetail)}
+          >view</button> 
+
+          <button 
+            style={show} 
+            onClick={() => setShowDetails(!showDetail)}
+          >hide</button> 
+
+        </div>
+        <div style={show}>
+          <div>
+            {blog.url}
+          </div>
+          <div>
+            likes {blog.likes}
+          </div>
+          <div>
+            {blog.user.name}
+          </div>
+        </div>
+
+    </div>  
+  )
+
+}
 
 export default Blog
