dc2a11565a49a4305e43bd16ef878e56558e7ecf
fgrullon <franfelix@gmail.com>
2023-03-17 17:30:43 -0400

    5.16 blog list tests step4 test checks if form when submited recieved the right input

--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -35,6 +35,7 @@ const BlogForm = ({ createBlog }) => {
             value={title}
             name="title"
             onChange={({ target }) => setTitle(target.value)}
+            className="title"
           />
         </div>
         <div>
@@ -44,6 +45,7 @@ const BlogForm = ({ createBlog }) => {
             value={author}
             name="author"
             onChange={({ target }) => setAuthor(target.value)}
+            className="author"
           />
         </div>
         <div>
@@ -53,10 +55,11 @@ const BlogForm = ({ createBlog }) => {
             value={url}
             name="url"
             onChange={({ target }) => setUrl(target.value)}
+            className="url"
           />
         </div>
       </div>
-      <button type="submit">create</button>
+      <button type="submit" className="submit">create</button>
     </form>
   )
 }
