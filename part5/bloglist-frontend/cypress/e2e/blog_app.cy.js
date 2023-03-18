e0a048e36de93832a5070bf2f79aaf934dae91c1
fgrullon <franfelix@gmail.com>
2023-03-18 07:26:35 -0400

    5.19 bloglist end to end testing step3 check if logged in user can create a new blog and the blog is listed in the blog list

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -2,7 +2,7 @@ describe('Blog app', function() {
 
   beforeEach(function() {
     cy.resetdb()
-    cy.createUser({ name : 'Frank', username : 'fgrullon', password : 'fullstackopen'})
+    cy.createUser({ name : 'Frank', username : 'fgrullon', password : 'fullstackopen' })
     cy.visit('http://localhost:3000')
   })
 
@@ -22,17 +22,40 @@ describe('Blog app', function() {
       cy.get('.success').should('have.css', 'color', 'rgb(0, 128, 0)')
     })
 
+    it('fails with wrong credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('fgrullon')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.contains('Wrong username or password')
+      cy.get('.error').should('contain', 'Wrong username or password')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+
   })
 
-  it('fails with wrong credentials', function() {
-    cy.contains('login').click()
-    cy.get('#username').type('fgrullon')
-    cy.get('#password').type('wrong')
-    cy.get('#login-button').click()
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.login({ username :'fgrullon', password : 'fullstackopen' })
+    })
 
-    cy.contains('Wrong username or password')
-    cy.get('.error').should('contain', 'Wrong username or password')
-    cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    it.only('A blog can be created', function() {
+
+      cy.get('.title').type('What Is JavaScript Made Of?')
+      cy.get('.author').type('Dan Abramov')
+      cy.get('.url').type('https://overreacted.io')
+
+      cy.get('.submit').click()
+
+      cy.get('.blog').should('contain', 'What Is JavaScript Made Of? Dan Abramov')
+
+      cy.get('.success').should('contain', 'a new blog What Is JavaScript Made Of? by Dan Abramov added')
+      cy.get('.success').should('have.css', 'color', 'rgb(0, 128, 0)')
+
+    })
   })
+
 
 })
+
--- part5/bloglist-frontend/cypress/support/commands.js
+++ part5/bloglist-frontend/cypress/support/commands.js
@@ -42,3 +42,12 @@ Cypress.Commands.add('login', ({ username, password }) => {
     cy.visit('http://localhost:3000')
   })
 })
+
+Cypress.Commands.add('createBlog', ({ username, password }) => {
+  cy.request('POST', 'http://localhost:3003/api/login', {
+    username, password
+  }).then(({ body }) => {
+    localStorage.setItem('loggedUser', JSON.stringify(body))
+    cy.visit('http://localhost:3000')
+  })
+})
