10e9d8541267875e70957b4c646ec8ed92624eb8
fgrullon <franfelix@gmail.com>
2023-03-29 16:22:00 -0400

    8.2 All books implemented query for returning all books and its details

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -106,12 +106,14 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 const resolvers = {
     Query: {
         bookCount: () => books.length,
-        authorCount: () => authors.length
+        authorCount: () => authors.length,
+        allBooks: () => books
     }
 }
 
