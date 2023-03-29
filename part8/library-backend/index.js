77e58436b481bcc2443cc6e09df331dfa669473a
fgrullon <franfelix@gmail.com>
2023-03-29 19:41:47 -0400

    8.7 Updating the birth year of an author implemented editAuthor which allow to update the birth year of an author

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -119,6 +119,13 @@ const typeDefs = `
         genres: [String]
     ): Book
   }
+
+  type Mutation{
+    editAuthor(
+        name: String! 
+        setBornTo: Int!
+    ): Author
+  }
 `
 const resolvers = {
     Query: {
@@ -148,6 +155,17 @@ const resolvers = {
             const book = {...args, id: uuid()}
             books = books.concat(book)
             return book
+        },
+        editAuthor: (root, args) => {
+            const author = authors.find(a => a.name === args.name)
+            if(!author){
+                return null
+            }
+            const updatedAuthor = {...author, born : args.setBornTo}
+            authors = authors.map(a => a.name !== args.name ? a : updatedAuthor)
+
+            return updatedAuthor
+
         }
     }
 }
