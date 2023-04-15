826e330da91896cea514d9893e49b306a37184d3
fgrullon <franfelix@gmail.com>
2023-04-14 23:05:11 -0400

    refactor working directory

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import DiagnoseRoutes from './routes/DiagnoseRoutes';
+import DiagnoseRoutes from '../src/routes/DiagnoseRoutes';
 
 const app = express();
 app.use(express.json());
@@ -9,7 +9,8 @@ app.get('/api/ping', (req, res) => {
     res.send('pong');
 });
 
-app.use('/api/diagnose', DiagnoseRoutes);
+app.get('/api/diagnose', DiagnoseRoutes);
+
 
 
 const PORT = 3001;
--- part9/patientor-backend/src/routes/DiagnoseRoutes.ts
+++ part9/patientor-backend/src/routes/DiagnoseRoutes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import diagnoseService from './services/DiagnoseService';
+import diagnoseService from '../services/DiagnoseService';
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send(diagnoseService.getDiagnoses());
 });
+
+export default router;
