98e4bbc26ee9f6bdff33fa041aa5e95f140b530b
fgrullon <franfelix@gmail.com>
2023-04-20 19:40:26 -0400

    update Part components add switch case for handleling component base on data provided

--- part9/typed-react/src/components/Part.tsx
+++ part9/typed-react/src/components/Part.tsx
@@ -1,10 +1,34 @@
 import { CoursePart } from '../types';
+import { unhandleSwitchCase } from '../utils';
 
 interface PartProps {
     part : CoursePart;
 }
 
+const Header = ({ text } : { text : string }) => {
+    return <h3>{ text }</h3>
+}
+
+const Description = ({ text }: { text : string}) => {
+    return <div>{ text }</div>
+}
+
 const Part = ( { part }: PartProps  ) => {
+
+    switch (part.kind) {
+        case 'basic':
+            return (
+                <>
+                    <Header text={`${part.name} ${part.exerciseCount}`}/>
+                    <Description text={part.description}/>
+                </>
+            );
+            
+            break;
+    
+        default:
+            break;
+    }
     console.log(part)
     return (<></>);
     // switch (props.kind) {
---
+++ part9/typed-react/src/utils.tsx
@@ -0,0 +1,6 @@
+const unhandleSwitchCase( value : never): never => {
+    throw new Error(`Undlandled switch case: ${JSON.stringify(value)}`);
+}
+
+export { unhandleSwitchCase }
+
