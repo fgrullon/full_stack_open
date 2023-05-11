71c238002b303e80aaf3a134684ce1601ac661fc
fgrullon <franfelix@gmail.com>
2023-05-11 18:14:19 -0400

    expand HospitalEntry component

--- part9/patientor/src/components/EntryPage/HospitalEntry.tsx
+++ part9/patientor/src/components/EntryPage/HospitalEntry.tsx
@@ -1,5 +1,32 @@
-const HospitalEntry = () => {
+import { Entry, Diagnosis } from '../../types';
 
+interface Props {
+    entry: Entry;
+    diagnoses: Diagnosis[];
+}
+  
+const HospitalEntry = ({ entry, diagnoses }: Props) => {
+
+    return (
+        <div
+        style={{ 
+            border : '2px solid black',
+            padding: '5px',
+            marginBottom: '10px' 
+
+        }}>
+            <div>
+                {entry.date} 
+            </div>
+            <div>{entry.description}</div>
+            <ul>
+                { 
+                    diagnoses.map(d => <li key={d.code}>{d.code} - {d.name}</li>) 
+                }
+            </ul>
+            <div>diagnose by {entry.specialist}</div>
+        </div>
+    )
 }
 
 export default HospitalEntry;
